2fb64014c3487c07e1ad9cf5931104a812418368
Wesley Thorsen <wesley@kinetik.care>
2024-12-01 20:02:27 -0500

    change localFallback to nonInteractive, add project and config prompt

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const { DopplerSDK } = require('@dopplerhq/node-sdk');
 const { getPassword } = require('keytar');
-const { parse: parseYaml } = require('yaml');
-const debug = require('debug')('serverless-doppler-secrets');
+const prompts = require('prompts');
 const { join, resolve } = require('node:path');
 const os = require('node:os');
 const fs = require('node:fs');
@@ -9,12 +8,7 @@ const fs = require('node:fs');
 class DopplerSecretsPlugin {
   constructor(serverless) {
     this.serverless = serverless;
-    this.getDopplerSecretsPromise = null;
-
-    this.getDopplerSecrets = this.getDopplerSecrets.bind(this);
-    this.getAvailableProjectIds = this.getAvailableProjectIds.bind(this);
-    this.getAvailableConfigIds = this.getAvailableConfigIds.bind(this);
-    this.getLocalDopplerSettings = this.getLocalDopplerSettings.bind(this);
+    this.getSecretsPromise = null;
 
     this.configurationVariablesSources = {
       doppler: {
@@ -28,7 +22,7 @@ class DopplerSecretsPlugin {
         token: { type: 'string' },
         project: { type: 'string' },
         config: { type: 'string' },
-        localFallback: { type: 'boolean' },
+        nonInteractive: { type: 'boolean' },
         stages: {
           type: 'object',
           additionalProperties: {
@@ -37,61 +31,59 @@ class DopplerSecretsPlugin {
               token: { type: 'string' },
               project: { type: 'string' },
               config: { type: 'string' },
-              localFallback: { type: 'boolean' },
+              nonInteractive: { type: 'boolean' },
             },
           },
         },
       },
     });
+
+    this.getDopplerSecrets = this.getDopplerSecrets.bind(this);
+    this.getLocalDopplerSettings = this.getLocalDopplerSettings.bind(this);
+    this.promptProject = this.promptProject.bind(this);
+    this.promptConfig = this.promptConfig.bind(this);
+    this.parseServerlessCliParams = this.parseServerlessCliParams.bind(this);
   }
 
   async resolveDopplerSecret({ address, options }) {
-    if (!this.getDopplerSecretsPromise) {
-      const cliParams = Object.fromEntries(
-        options.param
-          ?.filter(p => p?.startsWith('doppler-'))
-          ?.map(p => p.split('='))
-          ?.map(s => [s[0], s[1]]) || [],
-      );
-
+    if (!this.getSecretsPromise) {
       const stage = this.serverless.configurationInput.provider.stage;
+      const cliDopplerParams = this.parseServerlessCliParams(options.param);
+      const slsDopplerConfig = this.serverless.configurationInput.doppler;
 
       const accessToken =
-        cliParams['doppler-token'] ??
-        this.serverless.configurationInput.doppler?.stages?.[stage]?.token ??
-        this.serverless.configurationInput.doppler?.token;
+        cliDopplerParams['doppler-token'] ??
+        slsDopplerConfig?.stages?.[stage]?.token ??
+        slsDopplerConfig?.token;
 
-      const projectId =
-        cliParams['doppler-project'] ??
-        this.serverless.configurationInput.doppler?.stages?.[stage]?.project ??
-        this.serverless.configurationInput.doppler?.project;
+      const specifiedProjectId =
+        cliDopplerParams['doppler-project'] ??
+        slsDopplerConfig?.stages?.[stage]?.project ??
+        slsDopplerConfig?.project;
 
-      const configId =
-        cliParams['doppler-config'] ??
-        this.serverless.configurationInput.doppler?.stages?.[stage]?.config ??
-        this.serverless.configurationInput.doppler?.config;
+      const specifiedConfigId =
+        cliDopplerParams['doppler-config'] ??
+        slsDopplerConfig?.stages?.[stage]?.config ??
+        slsDopplerConfig?.config;
 
-      // TODO: convert string to boolean
-      let localFallback =
-        cliParams['doppler-local-fallback'] ??
-        this.serverless.configurationInput.doppler?.stages?.[stage]?.localFallback ??
-        this.serverless.configurationInput.doppler?.localFallback;
+      let nonInteractive =
+        cliDopplerParams['doppler-non-interactive'] ??
+        slsDopplerConfig?.stages?.[stage]?.nonInteractive ??
+        slsDopplerConfig?.nonInteractive;
 
-      debug('Resolving Doppler secrets');
-      debug('stage: %s', stage);
-      debug('accessToken: %s', accessToken);
-      debug('projectId: %s', projectId);
-      debug('configId: %s"', configId);
+      if (nonInteractive === false) {
+        nonInteractive = !process.stdin.isTTY;
+      }
 
-      this.getDopplerSecretsPromise = this.getDopplerSecrets({
+      this.getSecretsPromise = this.getDopplerSecrets({
         accessToken,
-        projectId,
-        configId,
-        localFallback,
+        specifiedProjectId,
+        specifiedConfigId,
+        interactive: !nonInteractive,
       });
     }
 
-    const secrets = await this.getDopplerSecretsPromise;
+    const secrets = await this.getSecretsPromise;
 
     if (!secrets[address]) {
       throw new Error(`could not resolve doppler secret "${address}"`);
@@ -102,45 +94,42 @@ class DopplerSecretsPlugin {
     };
   }
 
-  async getDopplerSecrets({ accessToken, projectId, configId, localFallback } = {}) {
+  async getDopplerSecrets({
+    accessToken,
+    specifiedProjectId,
+    specifiedConfigId,
+    nonInteractive,
+  } = {}) {
     const localSettings = await this.getLocalDopplerSettings();
 
-    accessToken ??= localFallback ? localSettings.accessToken : undefined;
+    accessToken ??= localSettings.accessToken;
     if (!accessToken) {
       throw new Error('missing doppler access token');
     }
 
     const doppler = new DopplerSDK({ accessToken });
 
-    const me = await doppler.auth.me();
+    const projectId = await this.promptProject({
+      doppler,
+      accessToken,
+      specifiedProjectId,
+      localProjectId: localSettings.projectId,
+      nonInteractive,
+    });
 
-    if (me.type_ === 'service_token') {
-      const [pid, ...otherPids] = await this.getAvailableProjectIds(doppler);
-      if (!pid) {
-        throw new Error('cannot find doppler project associated with service token');
-      }
-      if (otherPids?.length) {
-        throw new Error('multiple doppler projects associated with service token');
-      }
-
-      const [cid, ...otherCids] = await this.getAvailableConfigIds(doppler, pid);
-      if (!cid) {
-        throw new Error('cannot find doppler config associated with service token');
-      }
-      if (otherCids?.length) {
-        throw new Error('multiple doppler configs associated with service token');
-      }
-
-      projectId = pid;
-      configId = cid;
-    }
-
-    projectId ??= localFallback ? localSettings.projectId : undefined;
     if (!projectId) {
       throw new Error('missing doppler project');
     }
 
-    configId ??= localFallback ? localSettings.configId : undefined;
+    const configId = await this.promptConfig({
+      doppler,
+      accessToken,
+      projectId,
+      specifiedConfigId,
+      localConfigId: localSettings.configId,
+      nonInteractive,
+    });
+
     if (!configId) {
       throw new Error('missing doppler config');
     }
@@ -152,16 +141,103 @@ class DopplerSecretsPlugin {
     );
   }
 
-  async getAvailableProjectIds(doppler) {
-    const projects = await doppler.projects.list();
+  async promptProject({
+    doppler,
+    accessToken,
+    specifiedProjectId,
+    localProjectId,
+    nonInteractive,
+  }) {
+    const getProjectsResponse = await doppler.projects.list({ perPage: 10_000 });
 
-    return projects.projects?.map(p => p.slug) || [];
+    if (!getProjectsResponse.projects?.length) {
+      // no projects available for token
+      throw new Error('no available doppler projects');
+    }
+
+    if (accessToken.startsWith('dp.st.') && getProjectsResponse.projects.length === 1) {
+      // service token provided and there is one project available, automatically use this project
+      return getProjectsResponse.projects[0].slug;
+    }
+
+    const availableProjects = getProjectsResponse.projects.map(p => p.slug);
+
+    if (specifiedProjectId && availableProjects.includes(specifiedProjectId)) {
+      // projectId was specified and it is available, automatically use this project
+      return specifiedProjectId;
+    }
+
+    // multiple projects available, but projectId isn't specified or it doesn't match available projects
+    // must prompt user to select a project
+
+    if (nonInteractive) {
+      // non interactive mode, must fail
+      throw new Error('project selection required');
+    }
+
+    const initialIndex = availableProjects.indexOf(localProjectId);
+
+    const { projectId } = await prompts([
+      {
+        type: 'select',
+        name: 'projectId',
+        message: 'Select a Doppler project',
+        choices: getProjectsResponse.projects.map(p => ({ title: p.slug, value: p.slug })),
+        initial: initialIndex >= 0 ? initialIndex : undefined,
+      },
+    ]);
+
+    return projectId;
   }
 
-  async getAvailableConfigIds(doppler, projectId) {
-    const configs = await doppler.configs.list(projectId);
+  async promptConfig({
+    doppler,
+    accessToken,
+    projectId,
+    specifiedConfigId,
+    localConfigId,
+    nonInteractive,
+  }) {
+    const getConfigsResponse = await doppler.configs.list(projectId, { perPage: 10_000 });
 
-    return configs.configs?.map(c => c.name) || [];
+    if (!getConfigsResponse.configs?.length) {
+      // no configs available for token
+      throw new Error('no available doppler configs');
+    }
+
+    if (accessToken.startsWith('dp.st.') && getConfigsResponse.configs.length === 1) {
+      // service token provided and there is one config available, automatically use this config
+      return getConfigsResponse.configs[0].name;
+    }
+
+    const availableConfigs = getConfigsResponse.configs.map(p => p.name);
+
+    if (specifiedConfigId && availableConfigs.includes(specifiedConfigId)) {
+      // configId was specified and it is available, automatically use this config
+      return specifiedConfigId;
+    }
+
+    // multiple configs available, but configId isn't specified or it doesn't match available configs
+    // must prompt user to select a config
+
+    if (nonInteractive) {
+      // non interactive mode, must fail
+      throw new Error('config selection required');
+    }
+
+    const initialIndex = availableConfigs.indexOf(localConfigId);
+
+    const { configId } = await prompts([
+      {
+        type: 'select',
+        name: 'configId',
+        message: 'Select a Doppler config',
+        choices: getConfigsResponse.configs.map(c => ({ title: c.name, value: c.name })),
+        initial: initialIndex >= 0 ? initialIndex : undefined,
+      },
+    ]);
+
+    return configId;
   }
 
   async getLocalDopplerSettings() {
@@ -171,7 +247,7 @@ class DopplerSecretsPlugin {
       return {};
     }
 
-    const dopplerConfig = parseYaml(fs.readFileSync(dopplerConfigPath).toString());
+    const dopplerConfig = await this.serverless.yamlParser.parse(dopplerConfigPath);
 
     const keychainAccount = getScopedValue(
       dopplerConfig.scoped,
@@ -217,6 +293,23 @@ class DopplerSecretsPlugin {
         path = resolve(path, '..');
       }
     }
+  }
+
+  parseServerlessCliParams(params) {
+    return Object.fromEntries(
+      params
+        ?.filter(s => s.startsWith('doppler-'))
+        ?.map(p => p.split('='))
+        ?.map(s => [s[0], s[1]])
+        ?.map(p => {
+          // parse boolean values
+          if (p[1] === 'true' || p[1] === 'false' || p[1] === undefined) {
+            const isTrue = p[1] === 'true' || p[1] === undefined;
+            return [p[0], isTrue];
+          }
+          return p;
+        }) || [],
+    );
   }
 }
 
